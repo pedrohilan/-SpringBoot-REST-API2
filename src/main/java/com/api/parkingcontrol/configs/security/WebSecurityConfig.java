ea462d75c25de7e7aec8e542b6c7b7804715b260
pedrohilan <pedro.hilan99@gmail.com>
2023-01-17 16:44:47 -0300

    adjust in authentication

--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -23,10 +26,10 @@ public class WebSecurityConfig{
         
 		http
         .authorizeHttpRequests((authz) -> authz
-        	//.requestMatchers(HttpMethod.POST, "/user").hasRole("ADMIN")
-            .anyRequest().authenticated()
-        ).csrf().disable().cors().configurationSource(request -> corsConfiguration.applyPermitDefaultValues())
-        .and().httpBasic();
+        	//.requestMatchers(HttpMethod.POST, "/login").permitAll()
+            .anyRequest().authenticated())
+				.csrf().disable().cors().configurationSource(request -> corsConfiguration.applyPermitDefaultValues()).and()
+				.httpBasic();
     return http.build();
 	}
 	
